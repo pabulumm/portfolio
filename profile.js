c7461826fbad21f3e2b2121b089a6fac67526700
Tucker Stone <tuckerstone16@gmail.com>
2016-05-18 01:24:33 -0700

    First commit for portfolio project

---
+++ profile.js
@@ -0,0 +1,29 @@
+$(function() {
+	$('#work-content').hide();
+	$('#bio-content').hide().delay(500).slideDown(1000, "swing").addClass('selected');
+
+	$('#bio-select').click(function() {
+		console.log('Clicked BIO link');
+		// $('#bio-content').slideUp(1000);
+		if (!$('#bio-content').hasClass('selected')) {
+			$('.selected').slideUp('fast', "swing").removeClass('selected');
+			$('#bio-content').slideDown("slow", "swing").addClass('selected');
+		}
+	});
+
+	$('#work-select').click(function() {
+		console.log('Clicked WORK link');
+		if (!$('#work-content').hasClass('selected')) {
+			$('.selected').slideUp('fast', "swing").removeClass('selected');
+			$('#work-content').delay(500).slideDown("slow", "swing").addClass('selected');
+		}
+
+	})
+
+
+	$('#skills-select').click(function() {
+		console.log('Clicked SKILLS link');
+		
+	})
+
+});
